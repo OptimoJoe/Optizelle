0d50d78e06dfe333d4e3e6a528405db8753d8e68
Joseph Young <joe@optimojoe.com>
2014-01-13 23:58:59 +0000

    I simplifed the unit test setup slightly.
    I simplifed the unit test setup slightly.  Basically, we can now just
    add a test and then the testing macro makes sure the right flags are
    set.

--- src/cmake/Modules/AddOptizelleTest.cmake
+++ src/cmake/Modules/AddOptizelleTest.cmake
@@ -1,14 +1,34 @@
 # Sets up and runs tests on standardard Optizelle unit tests 
 macro(add_optizelle_test files executable)
-    file(GLOB_RECURSE units ${CMAKE_CURRENT_SOURCE_DIR} ${files})
-    list(SORT units)
-    list(LENGTH units len1)
-    math(EXPR len2 "${len1} - 1")
-    foreach(index RANGE ${len2})
-        list(GET units ${index} unit)
-        add_test("Execution_of_${unit}" ${executable} ${unit})
-        add_test("Solution_to_${unit}"
-            "${CMAKE_BINARY_DIR}/src/cpp/unit/utility/diff_restart"
-            ${unit} solution.json)
-    endforeach()
+
+    # Make sure that tests are enabled
+    if(ENABLE_CPP_UNIT)
+
+        # Grab all of the different .json test files
+        file(GLOB_RECURSE units ${CMAKE_CURRENT_SOURCE_DIR} ${files})
+
+        # Make sure we run everything in order
+        list(SORT units)
+
+        # We start indexing from 0, but the length parameter starts at1.
+        # We do the math here to fix it.
+        list(LENGTH units len1)
+        math(EXPR len2 "${len1} - 1")
+
+        # Loop over each of the tests
+        foreach(index RANGE ${len2})
+
+            # Grab the particular unit test
+            list(GET units ${index} unit)
+
+            # Run the optimization
+            add_test("Execution_of_${unit}" ${executable} ${unit})
+
+            # Diff the result of the optimization against the known solution
+            add_test("Solution_to_${unit}"
+                "${CMAKE_BINARY_DIR}/src/cpp/unit/utility/diff_restart"
+                ${unit} solution.json)
+        endforeach()
+    endif()
+
 endmacro(add_optizelle_test)
