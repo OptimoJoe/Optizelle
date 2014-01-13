c31ea1f93ab445eb4bf63db52f7bfc1273696f30
Joseph Young <joe@optimojoe.com>
2014-01-13 22:17:35 +0000

    I further simplified the examples and tests.
    I further simplified the examples and tests.  First, I eliminated all of
    the plain example json files.  Basically, some of these were getting out
    of date since I wasn't actually running them during the unit tests.
    Instead, I just install all of the algorithm specific json files with
    the examples and run the unit tests on all of these.  Techncially, these
    json files contain information not necessary to run the file; they also
    include information for the unit test.  However, that really shouldn't
    be such a big deal.  In addition, I modified how the sdpa_sparse_format
    tests are run to, in theory, make it easier to add additional tests
    later on.  Finally, for whatever reason, it didn't look like I was
    correctly not adding unit tests if the user wan't examples, but not unit
    tests.  I believe the scripts should be fixed now.

--- src/cmake/Modules/AddSDPATest.cmake
+++ src/cmake/Modules/AddSDPATest.cmake
@@ -2,18 +2,20 @@
 macro(add_sdpa_test dats params executable)
     file(GLOB_RECURSE dats ${CMAKE_CURRENT_SOURCE_DIR} ${dats})
     list(SORT dats)
-    file(GLOB_RECURSE units ${CMAKE_CURRENT_SOURCE_DIR} ${params})
-    list(SORT units)
+    file(GLOB_RECURSE params ${CMAKE_CURRENT_SOURCE_DIR} ${params})
+    list(SORT params)
     list(LENGTH dats len1)
     math(EXPR len2 "${len1} - 1")
     foreach(index RANGE ${len2})
         list(GET dats ${index} dat)
-        list(GET units ${index} unit)
-        add_test("Execution_of_${unit}" ${executable} ${dat}
-            ${CMAKE_CURRENT_SOURCE_DIR}/sdpa_sparse_format_phase1.json
-            ${CMAKE_CURRENT_SOURCE_DIR}/sdpa_sparse_format.json)
-        add_test("Solution_to_${unit}"
+        math(EXPR phase1_idx "2 * ${index}")
+        math(EXPR phase2_idx "2 * ${index} + 1")
+        list(GET params ${phase1_idx} phase1)
+        list(GET params ${phase2_idx} phase2)
+        add_test("Execution_of_${dat}"
+            ${executable} ${dat} ${phase1} ${phase2})
+        add_test("Solution_to_${dat}"
             "${CMAKE_BINARY_DIR}/src/cpp/unit/utility/diff_restart"
-            ${unit} solution.json)
+            ${phase2} solution.json)
     endforeach()
 endmacro(add_sdpa_test)
