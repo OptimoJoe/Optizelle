f12e3af31531b85c4d7cb16a35d99e50e7393406
Joseph Young <joe@optimojoe.com>
2014-02-26 00:20:23 +0000

    I cleaned up some of the variables in the...
    I cleaned up some of the variables in the MATLAB build scripts.  Also, I
    modified the build setup so that we can more easily find and use
    archives for jsoncpp and blas/lapack when building them for the user as
    a convenience.

--- src/cmake/Modules/FindMATLAB.cmake
+++ src/cmake/Modules/FindMATLAB.cmake
@@ -23,11 +23,11 @@ find_package_handle_standard_args(MATLAB DEFAULT_MSG
 mark_as_advanced(MATLAB_INCLUDE_DIR MATLAB_LIBRARY)
 
 # Find the mex file extension
-set(MEX_UNDEFINED "MATLAB_MEX_EXTENSION-UNDEFINED")
-set(MATLAB_MEX_EXTENSION ${MEX_UNDEFINED} CACHE STRING
+set(MATLAB_MEX_EXTENSION_UNDEFINED "MATLAB_MEX_EXTENSION-UNDEFINED")
+set(MATLAB_MEX_EXTENSION ${MATLAB_MEX_EXTENSION_UNDEFINED} CACHE STRING
     "MATLAB mex file extension.  Type 'mexext' in MATLAB.")
 # Make sure that it is defined and throw an error message if it is not
-if(MATLAB_MEX_EXTENSION STREQUAL MEX_UNDEFINED) 
+if(MATLAB_MEX_EXTENSION STREQUAL MATLAB_MEX_EXTENSION_UNDEFINED) 
     mark_as_advanced(CLEAR MATLAB_MEX_EXTENSION)
     message(FATAL_ERROR
         "The MATLAB mex extension must be defined in the variable MATLAB_MEX_EXTENSION.  Type 'mexext' in MATLAB.")
@@ -35,11 +35,11 @@ endif()
 mark_as_advanced(FORCE MATLAB_MEX_EXTENSION)
 
 # Find the MATLAB executable 
-set(MEX_UNDEFINED "MATLAB_EXECUTABLE-UNDEFINED")
-set(MATLAB_EXECUTABLE ${MEX_UNDEFINED} CACHE STRING
+set(MATLAB_EXECUTABLE_UNDEFINED "MATLAB_EXECUTABLE-UNDEFINED")
+set(MATLAB_EXECUTABLE ${MATLAB_EXECUTABLE_UNDEFINED} CACHE FILEPATH 
     "MATLAB executable with full path.")
 # Make sure that it is defined and throw an error message if it is not
-if(MATLAB_EXECUTABLE STREQUAL MEX_UNDEFINED) 
+if(MATLAB_EXECUTABLE STREQUAL MATLAB_EXECUTABLE_UNDEFINED) 
     mark_as_advanced(CLEAR MATLAB_EXECUTABLE)
     message(FATAL_ERROR
         "The MATLAB executable with full path must be defined in the variable MATLAB_EXECUTABLE.")
@@ -47,11 +47,11 @@ endif()
 mark_as_advanced(FORCE MATLAB_EXECUTABLE)
 
 # Find the MATLAB executable 
-set(MEX_UNDEFINED "MATLAB_RUN_FLAG-UNDEFINED")
-set(MATLAB_RUN_FLAG ${MEX_UNDEFINED} CACHE STRING
+set(MATLAB_RUN_FLAG_UNDEFINED "MATLAB_RUN_FLAG-UNDEFINED")
+set(MATLAB_RUN_FLAG ${MATLAB_RUN_FLAG_UNDEFINED} CACHE STRING
     "MATLAB command line flag to run a script (-r in MATLAB and --eval in Octave).")
 # Make sure that it is defined and throw an error message if it is not
-if(MATLAB_RUN_FLAG STREQUAL MEX_UNDEFINED) 
+if(MATLAB_RUN_FLAG STREQUAL MATLAB_RUN_FLAG_UNDEFINED) 
     mark_as_advanced(CLEAR MATLAB_RUN_FLAG)
     message(FATAL_ERROR
         "The MATLAB command line flag to run a script (-r in MATLAB and --eval in Octave) must be defined in the variable MATLAB_RUN_FLAG.")
