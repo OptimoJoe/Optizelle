2e9002b520e96c7c020cd93ee0d74226e29c0dcd
Joseph Young <joe@optimojoe.com>
2015-02-10 14:31:18 +0100

    Added SQL cone constraint support to MATLAB
    I added basic support for SQL cone constraints to MATLAB.  This has
    existed for sometime in the C++ code.  Now, that being said, there are
    still some gaps.  I've not completed the serialization routines for this
    problem yet.  Part of the reason is that the SQL code in C++ uses a
    different way to store the information that MATLAB.  Basically, in C++
    it's convenient to have everything in memory laid out in a columnwise
    vector since we're going to pass the operations to LAPACK anyway.
    However, in MATLAB, this is a pain to deal with because we have to
    constantly reshape our information.  Therefore, the serialization for
    the routines would look different, which goes against our general theme
    of letting someone take a serialized problem from one language and move
    it to another.  As such, this will require some more thinking.  In
    addition, the behavior of the implementations is slightly different due
    to different implemenations of the operators.  Since we're in MATLAB, we
    can just use things like eigs for the generalized eigenvalue problem,
    which will give a different behavior that the hand coded one in C++.

--- src/examples/simple_quadratic_cone/simple_quadratic_cone.m
+++ src/examples/simple_quadratic_cone/simple_quadratic_cone.m
@@ -1,9 +1,8 @@
-% Optimize a simple optimization problem with an optimal solution
-% of (1/3,1/3)
-function simple_inequality(fname)
+% Optimize a simple problem with an optimal solution of (2.5,2.5)
+function simple_quadratic_cone(fname)
     % Read in the name for the input file
     if nargin ~=1
-        error('simple_inequality <parameters>');
+        error('simple_quadratic_cone <parameters>');
     end
 
     % Execute the optimization
@@ -17,17 +16,17 @@ end
 
 % Define a simple objective where 
 % 
-% f(x,y)=(x+1)^2+(y+1)^2
+% f(x,y)=(x-3)^2+(y-2)^2
 %
 function self = MyObj()
 
     % Evaluation 
-    self.eval = @(x) sq(x(1)+1.)+sq(x(2)+1.);
+    self.eval = @(x) sq(x(1)-3.)+sq(x(2)-2.);
 
     % Gradient
     self.grad = @(x) [
-        2.*x(1)+2.;
-        2.*x(2)+2.];
+        2.*x(1)-6;
+        2.*x(2)-4];
 
     % Hessian-vector product
     self.hessvec = @(x,dx) [
@@ -35,30 +34,46 @@ function self = MyObj()
         2.*dx(2)];
 end
 
-% Define simple inequalities 
+% Define a simple SOCP inequality 
 %
-% h(x,y)= [ x + 2y >= 1 ] 
-%         [ 2x + y >= 1 ] 
+% h(x,y) = [ y >= |x| ] 
+% h(x,y) =  (y,x) >=_Q 0
 %
 function self = MyIneq()
 
     % y=h(x) 
-    self.eval = @(x) [
-        x(1)+2.*x(2)-1. ;
-        2.*x(1)+x(2)-1. ];
+    self.eval = @(x)MyIneq_eval(x);
 
     % y=h'(x)dx
-    self.p = @(x,dx) [
-        dx(1)+2.*dx(2) ;
-        2.*dx(1)+dx(2) ];
+    self.p = @(x,dx)MyIneq_p(x,dx);
 
     % z=h'(x)*dy
     self.ps = @(x,dy) [
-        dy(1)+2.*dy(2) ;
-        2.*dy(1)+dy(2) ];
+        dy.data{1}(2);
+        dy.data{1}(1)];
 
     % z=(h''(x)dx)*dy
-    self.pps = @(x,dx,dy) zeros(2,1); 
+    self.pps = @(x,dx,dy) [
+        0;
+        0];
+end
+
+% y=h(x) 
+function y=MyIneq_eval(x)
+    global Optizelle;
+    y = Optizelle.SQL.create([Optizelle.Cone.Quadratic],[2]);
+    y.data{1} = [
+        x(2);
+        x(1)];
+end
+
+% y=h'(x)dx
+function y=MyIneq_p(x,dx)
+    global Optizelle;
+    y = Optizelle.SQL.create([Optizelle.Cone.Quadratic],[2]);
+    y.data{1} = [
+        dx(2);
+        dx(1)];
 end
 
 % Actually runs the program
@@ -68,20 +83,20 @@ function main(fname)
     global Optizelle;
     setupOptizelle();
 
-    % Generate an initial guess 
-    x = [2.1;1.1];
+    % Generate an initial guess for the primal
+    x = [1.2; 3.1];
 
-    % Allocate memory for the inequality multiplier 
-    z = [0.;0.];
+    % Generate an initial guess for the dual
+    z = Optizelle.SQL.create([Optizelle.Cone.Quadratic],[2]);
 
     % Create an optimization state
     state=Optizelle.InequalityConstrained.State.t( ...
-        Optizelle.Rm,Optizelle.Rm,Optizelle.Messaging,x,z);
+        Optizelle.Rm,Optizelle.SQL,Optizelle.Messaging,x,z);
 
     % Read the parameters from file
     state=Optizelle.json.InequalityConstrained.read( ...
-        Optizelle.Rm,Optizelle.Rm,Optizelle.Messaging,fname,state);
-
+        Optizelle.Rm,Optizelle.SQL,Optizelle.Messaging,fname,state);
+    
     % Create a bundle of functions
     fns=Optizelle.InequalityConstrained.Functions.t;
     fns.f=MyObj();
@@ -89,7 +104,7 @@ function main(fname)
 
     % Solve the optimization problem
     state=Optizelle.InequalityConstrained.Algorithms.getMin( ...
-        Optizelle.Rm,Optizelle.Rm,Optizelle.Messaging,fns,state);
+        Optizelle.Rm,Optizelle.SQL,Optizelle.Messaging,fns,state);
 
     % Print out the reason for convergence
     fprintf('The algorithm converged due to: %s\n', ...
@@ -97,8 +112,8 @@ function main(fname)
 
     % Print out the final answer
     fprintf('The optimal point is: (%e,%e)\n',state.x(1),state.x(2));
-    
+
     % Write out the final answer to file
     Optizelle.json.InequalityConstrained.write_restart( ...
-        Optizelle.Rm,Optizelle.Rm,Optizelle.Messaging,'solution.json',state);
+        Optizelle.Rm,Optizelle.SQL,Optizelle.Messaging,'solution.json',state);
 end
--- src/examples/simple_sdp_cone/simple_sdp_cone.m
+++ src/examples/simple_sdp_cone/simple_sdp_cone.m
@@ -1,64 +1,75 @@
-% Optimize a simple optimization problem with an optimal solution
-% of (1/3,1/3)
-function simple_inequality(fname)
+% Optimize a simple problem with an optimal solution of (0.5,.25)
+
+function simple_sdp_cone(fname)
     % Read in the name for the input file
     if nargin ~=1
-        error('simple_inequality <parameters>');
+        error('simple_sdp_cone <parameters>');
     end
 
     % Execute the optimization
     main(fname);
 end
 
-% Squares its input
-function z = sq(x)
-    z=x*x;
-end
-
 % Define a simple objective where 
 % 
-% f(x,y)=(x+1)^2+(y+1)^2
+% f(x,y)=-x+y
 %
 function self = MyObj()
 
     % Evaluation 
-    self.eval = @(x) sq(x(1)+1.)+sq(x(2)+1.);
+    self.eval = @(x) -x(1)+x(2) 
 
     % Gradient
     self.grad = @(x) [
-        2.*x(1)+2.;
-        2.*x(2)+2.];
+        -1.;
+        1.];
 
     % Hessian-vector product
     self.hessvec = @(x,dx) [
-        2.*dx(1);
-        2.*dx(2)];
+        0;
+        0];
 end
 
-% Define simple inequalities 
+% Define a simple SDP inequality 
 %
-% h(x,y)= [ x + 2y >= 1 ] 
-%         [ 2x + y >= 1 ] 
+% h(x,y) = [ y x ] >= 0
+%          [ x 1 ]
 %
 function self = MyIneq()
 
     % y=h(x) 
-    self.eval = @(x) [
-        x(1)+2.*x(2)-1. ;
-        2.*x(1)+x(2)-1. ];
+    self.eval = @(x)MyIneq_eval(x);
 
     % y=h'(x)dx
-    self.p = @(x,dx) [
-        dx(1)+2.*dx(2) ;
-        2.*dx(1)+dx(2) ];
+    self.p = @(x,dx)MyIneq_p(x,dx);
 
     % z=h'(x)*dy
     self.ps = @(x,dy) [
-        dy(1)+2.*dy(2) ;
-        2.*dy(1)+dy(2) ];
+        dy.data{1}(2,1)+dy.data{1}(1,2);
+        dy.data{1}(1,1)];
 
     % z=(h''(x)dx)*dy
-    self.pps = @(x,dx,dy) zeros(2,1); 
+    self.pps = @(x,dx,dy) [
+        0;
+        0];
+end
+
+% y=h(x) 
+function y=MyIneq_eval(x)
+    global Optizelle;
+    y = Optizelle.SQL.create([Optizelle.Cone.Semidefinite],[2]);
+    y.data{1} = [
+        x(2) x(1);
+        x(1) 1.  ];
+end
+
+% y=h'(x)dx
+function y=MyIneq_p(x,dx)
+    global Optizelle;
+    y = Optizelle.SQL.create([Optizelle.Cone.Semidefinite],[2]);
+    y.data{1} = [
+        dx(2) dx(1);
+        dx(1) 0.   ];
 end
 
 % Actually runs the program
@@ -68,20 +79,20 @@ function main(fname)
     global Optizelle;
     setupOptizelle();
 
-    % Generate an initial guess 
-    x = [2.1;1.1];
+    % Generate an initial guess for the primal
+    x = [1.2; 3.1];
 
-    % Allocate memory for the inequality multiplier 
-    z = [0.;0.];
+    % Generate an initial guess for the dual
+    z = Optizelle.SQL.create([Optizelle.Cone.Semidefinite],[2]);
 
     % Create an optimization state
     state=Optizelle.InequalityConstrained.State.t( ...
-        Optizelle.Rm,Optizelle.Rm,Optizelle.Messaging,x,z);
+        Optizelle.Rm,Optizelle.SQL,Optizelle.Messaging,x,z);
 
     % Read the parameters from file
     state=Optizelle.json.InequalityConstrained.read( ...
-        Optizelle.Rm,Optizelle.Rm,Optizelle.Messaging,fname,state);
-
+        Optizelle.Rm,Optizelle.SQL,Optizelle.Messaging,fname,state);
+    
     % Create a bundle of functions
     fns=Optizelle.InequalityConstrained.Functions.t;
     fns.f=MyObj();
@@ -89,7 +100,7 @@ function main(fname)
 
     % Solve the optimization problem
     state=Optizelle.InequalityConstrained.Algorithms.getMin( ...
-        Optizelle.Rm,Optizelle.Rm,Optizelle.Messaging,fns,state);
+        Optizelle.Rm,Optizelle.SQL,Optizelle.Messaging,fns,state);
 
     % Print out the reason for convergence
     fprintf('The algorithm converged due to: %s\n', ...
@@ -97,8 +108,8 @@ function main(fname)
 
     % Print out the final answer
     fprintf('The optimal point is: (%e,%e)\n',state.x(1),state.x(2));
-    
+
     % Write out the final answer to file
     Optizelle.json.InequalityConstrained.write_restart( ...
-        Optizelle.Rm,Optizelle.Rm,Optizelle.Messaging,'solution.json',state);
+        Optizelle.Rm,Optizelle.SQL,Optizelle.Messaging,'solution.json',state);
 end
