a391d8b1dcd4ff57b0469a5b0d3145d7285c2cdb
Joseph Young <joe@optimojoe.com>
2016-02-23 20:45:10 -0700

    Added test for neg curve, but last point not safe
    I added a test where we hit negative curvature where the second to last
    point was safe, but the last point was not.  Therefore, we do a
    safeguard step from the second to last point to the last point.

---
+++ src/unit/linear_algebra/tcg_safeguard_last_not_safe.cpp
@@ -0,0 +1,64 @@
+// Run TCG with a safeguard where the last point prior to convergence violates
+// the safeguard.  Specifically
+//
+// x_offset = (0,0,0,0,0)
+// w = (0,0,0,0,-1)
+// lb = -10 
+//
+// The sequence of solutions produced by CG is approximately
+//
+// 0 0 0 0 0
+// 0 0 0 0 1/2
+// 0 0 0 -1/3 2/3
+// 0 0 1/4 -1/2 3/4
+// 0 -1/5 2/5 -3/5 4/5
+// 67000 -53000 40000 -27000 13000
+//
+// Hence, the sequence <x + x_offset,w> is
+//
+// -1/2, -2/3, -3/4, -4/5, -13000 
+//
+// As such, we require that <x + x_offset,w> >= -10, which means that we
+// violate the safeguard on the last step.  However, the step prior to that is
+// feasible, so we just do a linesearch as far as possible after detecting
+// negative curvature.
+
+#include "linear_algebra.h"
+#include "spaces.h"
+
+int main() {
+    // Setup the problem 
+    auto setup = Unit::tcg <Real,XX> ();
+
+    // Problem setup 
+    setup.A = std::make_unique <Matrix>(
+        Unit::Matrix <Real>::mostly_dd_indef(setup.m));
+    setup.b = std::make_unique <Vector> (Unit::Vector <Real>::elast(setup.m));
+    auto lb = Real(-10.); 
+    auto x = Unit::Vector<Real>::zero(setup.m);
+    auto w = std::vector <Real> {0,0,0,0,-1};
+    setup.safeguard = std::make_unique<Optizelle::SafeguardSimplified<Real,XX>>(
+        Unit::Safeguard <Real,XX>::lower(x,lb,w));
+
+    // Make the TR radius large enough to not constrain ourselves until we
+    // hit the negative curvature. 
+    setup.delta = 1e5; 
+
+    // Target solutions
+    setup.iter_star = 5;
+    setup.stop_star = Optizelle::TruncatedStop::NegativeCurvature;
+    setup.safeguard_failed_star = 0;
+
+    // Tests
+    Unit::reset_checks <Real,XX> (setup);
+    setup.check_iter = true;
+    setup.check_stop = true;
+    setup.check_safeguard_failed = true;
+    setup.check_safeguard_alpha = true;
+
+    // Check the solver 
+    Unit::run_and_verify <Real,XX> (setup);
+
+    // Declare success
+    return EXIT_SUCCESS;
+}
