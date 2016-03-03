a802c98047bcf0025c8e38626bf6a82d427e589d
Joseph Young <joe@optimojoe.com>
2016-03-03 15:33:51 -0700

    Added QN test where Newton safe, Cauchy unsafe
    I added quasinormal step test where the Newton step is safe, but the
    Cauchy step is unsafe.

--- src/unit/core/qn_newton_cauchy_unsafe.cpp
+++ src/unit/core/qn_newton_cauchy_unsafe.cpp
@@ -1,7 +1,5 @@
-// Test the quasinormal steps ability to exit on the Newton step.  Here, we
-// find the intersection of two circles where the first has the center at (1,0)
-// and the second has the center at (1,1).  The intersection occurs at
-// (1+-sqrt(3)/2,1/2).
+// Test the quasinormal steps ability to exit on the Newton step when the
+// Cauchy point violates the inequality constraint. 
 
 #include "optizelle/optizelle.h"
 #include "optizelle/vspaces.h"
@@ -18,10 +16,15 @@ int main(int argc,char* argv[]){
     auto setup = Unit <Real>::QN(x,y);
     setup.g.reset(new Unit <Real>::Constraint::CircleIntersection(1.,0.,1.,1.));
 
+    // Create an inequality constraint to cut off the Cauchy point 
+    auto c = setup.zeta * setup.gamma;
+    setup.h.reset(new Unit <Real>::Constraint::Box(
+        {-1.,-1.},{0.1/c,10.}));
+
     // Set the targets
     setup.qn_stop_star = Optizelle::QuasinormalStop::Newton;
     setup.dx_n_star.reset(new std::vector <Real> {0.0,0.5});
-    setup.dx_ncp_star.reset(new std::vector <Real> {0.2,0.2});
+    setup.dx_ncp_star.reset(new std::vector <Real> {0.1,0.1});
     X::axpy(Real(-1.),x,*setup.dx_n_star);
     
     // Set what tests we want
