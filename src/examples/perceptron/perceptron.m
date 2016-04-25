8b729cb118a5a9553cfdf368af90e9c5ebf1c040
Joseph Young <joe@optimojoe.com>
2016-04-25 14:56:00 -0600

    Added some missing files to help with build
    I added some missing files from the last check-in.  Basically, they're
    stubs to help with the build.  By default, I automatically install files
    that correspond to the example name.  Maybe this isn't a good idea, but
    it worked for everything except for these two examples.

---
+++ src/examples/perceptron/perceptron.m
@@ -0,0 +1,6 @@
+% In this example, we demonstrate how to use Optizelle to train multilayer
+% perceptrons.  For specific problem setups, see:
+%
+% example_1d.m
+% example_1d_extruded.m
+% example_2d.m
